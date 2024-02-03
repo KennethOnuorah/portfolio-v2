738094e116354eb45c47e79d57f7c2a567bfdc6d
kenneth4832@gmail.com <kenneth4832@gmail.com>
2024-02-02 20:09:24 -0600

    Fix for deployment

--- src/lib/hooks/useViewportDimensions.tsx
+++ src/lib/hooks/useViewportDimensions.tsx
@@ -1,21 +1,24 @@
 import { useState, useEffect } from 'react'
 
-function getViewportDimensions() {
-  if(typeof window !== undefined) {
-    const { innerWidth: width, innerHeight: height } = window
-    return {
-      width,
-      height
-    }
-  }
-}
+// function getViewportDimensions() {
+//   if(typeof window !== undefined) {
+//     const { innerWidth: width, innerHeight: height } = window
+//     return {
+//       width,
+//       height
+//     }
+//   }
+// }
 
 const useViewportDimensions = () => {
-  const [dimensions, setDimensions] = useState(getViewportDimensions())
+  const [dimensions, setDimensions] = useState({})
 
   useEffect(() => {
     function handleResize() {
-      setDimensions(getViewportDimensions())
+      if(typeof window !== undefined) {
+        const { innerWidth: width, innerHeight: height } = window
+        setDimensions({width, height})
+      }
     }
     window.addEventListener('resize', handleResize)
     return () => window.removeEventListener('resize', handleResize)
