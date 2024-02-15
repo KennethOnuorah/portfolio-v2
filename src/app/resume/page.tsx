3cef913f2cdcffd0a32e2bd4c9710f9c5ebb8c40
kenneth4832@gmail.com <kenneth4832@gmail.com>
2024-02-14 20:03:41 -0600

    Some fixes for 'failed pdf render' issue

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,18 +1,18 @@
 'use client'
 
+import { useResizeDetector } from 'react-resize-detector';
+import { Document, Page, pdfjs } from "react-pdf"
+import "react-pdf/dist/esm/Page/TextLayer.css";
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`
+
 import { useState } from "react";
 import { Metadata } from "next";
-import { useResizeDetector } from "react-resize-detector";
-import { Document, Page, pdfjs } from "react-pdf"
 
 import { Typography } from "@mui/material"
 
 import ResumeSkeleton from "./components/ResumeSkeleton";
 import useViewportDimensions from "@/lib/hooks/useViewportDimensions";
 import getLastModifiedDate from "@/lib/getLastModifiedDate";
-
-import "react-pdf/dist/esm/Page/TextLayer.css";
-pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`
 
 import { TbDownload as Download } from "react-icons/tb";
 
@@ -31,7 +31,7 @@ export default function ResumePage() {
   const { width, ref } = useResizeDetector()
 
   const updateLastUpdatedDate = async() => {
-    const pdf = await pdfjs.getDocument('/docs/resume.pdf').promise
+    const pdf = await pdfjs.getDocument('/Resume.pdf').promise
     const data = await pdf.getMetadata()
     setLastUpdated(Object.entries(data.info)[10][1])
   }
@@ -49,7 +49,7 @@ export default function ResumePage() {
       <div className="flex justify-center">
         <a 
           className="transition-all flex items-center gap-1 bg-gray-300 p-2 rounded-md hover:bg-gray-100 my-4"
-          href={"/docs/Resume.pdf"}
+          href={'/Resume.pdf'}
           target="_blank"
         >
           <Download/>
@@ -59,15 +59,15 @@ export default function ResumePage() {
       {isLoading && <ResumeSkeleton/>}
       <div ref={ref} className={`flex justify-center ${vWidth > 600 ? 'mx-28' : 'm-0'}`}>
         <Document
-          file={"/docs/Resume.pdf"}
-          className={'shadow-xl'}
-          onLoadSuccess={() => {
-            updateLastUpdatedDate()
-            setIsLoading(false)
-          }}
-        >
-          <Page pageNumber={1} width={width} renderAnnotationLayer={false}/>
-        </Document>
+            file={"/Resume.pdf"}
+            className={'shadow-xl'}
+            onLoadSuccess={() => {
+              updateLastUpdatedDate()
+              setIsLoading(false)
+            }}
+          >
+            <Page pageNumber={1} width={width} renderAnnotationLayer={false}/>
+          </Document>
       </div>
     </main>
   )
